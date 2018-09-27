d61e34e1e3807d51be08d2bbaafb619bf8d32ff9
Isaac Lewis <izejames@hotmail>
2018-09-27 11:49:01 +0000

    Showing only names with less than 12 characters

--- directory.rb
+++ directory.rb
@@ -26,7 +26,7 @@ puts "------------"
 end
 def print(students)
    students.each_with_index do |student,i|
-    puts "#{i} #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with? 'A'
+    puts "#{i} #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
   end
 end
 def print_footer(student)
